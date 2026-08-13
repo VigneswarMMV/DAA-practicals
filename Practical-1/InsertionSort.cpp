54775f5b3e5ee8e780be720cc76ebe2837fc2a9f
Vigneswar Mavillapalli <srivigneswar03@gmail.com>
2026-08-14 01:15:05 +0530

    Added Insertion Sort

---
+++ Practical-1/InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {7, 6, 10, 5, 9, 2, 1, 15, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
